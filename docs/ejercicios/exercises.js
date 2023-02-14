c34424d62d978ee0e911c55ad6e4a2c14971a438
eusbeidy <eurimary@gmail.com>
2023-02-13 22:05:58 -0500

    se agrego exercises

---
+++ docs/ejercicios/exercises.js
@@ -0,0 +1,31 @@
+'use strict'
+// Programa que retorne el indice de masa corporal dado un peso en kg y una estatura en cms.
+
+
+/* Programa que retorne TRUE si un string dado es s/si, que retorne FALSE si el
+string dado es n/no, de otra manera retornar null. */
+
+/*const  retornar = (str) => {
+    const string2 = str.toLowerCase().trim();
+    if (string2 === 's' || string2 === 'si') {
+        return true
+    } if (string2 === 'n' || string2 === 'no') {
+        return false
+    } else {
+        return null
+    }
+};
+console.log(retornar('s'));
+console.log(retornar('n'));*/
+
+
+// Programa que retorne un string dado con la primer letra en mayúscula.
+
+const retornestr = (str) => {
+    const mayuscula = str.toLowerCase();
+    if (mayuscula === "hola") {
+        return ('Hola '.toLowerCase())
+    }
+   
+}
+console.log(retornestr(' hola'))
